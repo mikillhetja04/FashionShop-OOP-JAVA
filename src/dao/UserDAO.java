25fc129385ee6ee014cefefd8fc47d7b96d84d30
unknown <tdhvtmd@gmail.com>
2026-03-19 15:21:38 +0700

    TV2: Completed secure authentication with SHA-256 hashing

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import DBpackage.DBConnection;
 import model.User;
+import utils.HashUtils;
 
 public class UserDAO {
 
@@ -22,9 +23,11 @@ public class UserDAO {
             Connection conn = DBConnection.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
             
+         
             // Truyền tham số để chống SQL Injection
             ps.setString(1, username);
-            ps.setString(2, password);
+         // Khi kiểm tra, cũng phải băm mật khẩu người dùng vừa nhập thì mới khớp với DB
+            ps.setString(2, HashUtils.hashPassword(password));
             
             ResultSet rs = ps.executeQuery();
             
@@ -48,15 +51,20 @@ public class UserDAO {
      * @return true nếu đăng ký thành công, false nếu thất bại (trùng username...)
      */
     public boolean registerUser(User u) {
-        // Mặc định khi đăng ký qua web/app sẽ là khách hàng (CUSTOMER)
+    	
+    	// Mặc định khi đăng ký qua web/app sẽ là khách hàng (CUSTOMER)
         String sql = "INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, 'CUSTOMER')";
         
         try {
             Connection conn = DBConnection.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
             
+            
             ps.setString(1, u.getUsername());
-            ps.setString(2, u.getPassword());
+         // Trước khi set vào PreparedStatement, hãy băm nó!
+        	ps.setString(2, HashUtils.hashPassword(u.getPassword()));
+            
+            
             ps.setString(3, u.getEmail());
             
             int result = ps.executeUpdate();
@@ -106,7 +114,7 @@ public class UserDAO {
 //    }
     public static void main(String[] args) {
         UserDAO dao = new UserDAO();
-        String targetUser = "khachhang_vip"; // Tên muốn đăng ký
+        String targetUser = "khachhang_vip1"; // Tên muốn đăng ký
 
         System.out.println("--- ĐANG KIỂM TRA TÊN ĐĂNG NHẬP ---");
         if (dao.isUsernameExists(targetUser)) {
@@ -116,8 +124,8 @@ public class UserDAO {
             
             User newUser = new User();
             newUser.setUsername(targetUser);
-            newUser.setPassword("matkhau123");
-            newUser.setEmail("vip@gmail.com");
+            newUser.setPassword("matkhau1234");
+            newUser.setEmail("vip1@gmail.com");
 
             if (dao.registerUser(newUser)) {
                 System.out.println("🎉 Đăng ký THÀNH CÔNG tài khoản: " + targetUser);
